775a5639a0c5f0771c6857c0b844d29009b878c7
Voletiyashasvigupta <97425703+Voletiyashasvigupta@users.noreply.github.com>
2022-07-02 18:46:21 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return 1     else:         return 0 a=int(input()) b=[] su=0 for i in range(1,a+1):     if prime(i)==1 :         b.append(i) for j in range(len(b)):     for k in range(len(b)):         if(j==k):             continue         else:             if(b[j]*b[k]==a):                 su+=1                 print(b[j],end=' ')                 break if(su!=2):     print("-1")

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,24 +1,26 @@
 def prime(a):
     c=0
-    for i in range(1,a):
-        if(a%i==0):
+    for i in range(1,a+1):
+        if a%i==0:
             c+=1
-    if(c==1):
+    if c==2:
         return 1
     else:
         return 0
 a=int(input())
-c=0
-for i in range(1,a):
-    if(prime(i)==1):
-        for j in range(1,a):
-            if(prime(j)==1):
-                if(i!=j):
-                    if(i*j==a):
-                        c=1
-                        print(i,j)
-                        break
-        if(c==1):
-            break
-if(c==0):
+b=[]
+su=0
+for i in range(1,a+1):
+    if prime(i)==1 :
+        b.append(i)
+for j in range(len(b)):
+    for k in range(len(b)):
+        if(j==k):
+            continue
+        else:
+            if(b[j]*b[k]==a):
+                su+=1
+                print(b[j],end=' ')
+                break
+if(su!=2):
     print("-1")
