de2abae446b42d09edb18da9f53a3bd5912b70c5
Voletiyashasvigupta <97425703+Voletiyashasvigupta@users.noreply.github.com>
2022-10-12 10:53:45 +0530

    m,n=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) a=set(a) b=set(b) c=[] for i in a:     if i in b:         c.append(i) for j in b:     if j in a:         c.append(j) print(len(c)//2)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,24 +1,13 @@
-a,b=map(int,input().split())
-arr=list(map(int,input().split()))
-arr2=list(map(int,input().split()))
-x=0
-for i in range(a):
-    for j in range(a):
-        if i!=j:
-            if arr[i]==arr[j]:
-                if arr[i]!=-1000:
-                    arr[j]=-1000
-for i in range(b):
-    for j in range(b):
-        if i!=j:
-            if arr2[i]==arr2[j] and arr2[i]!=-100:
-                arr2[j]=-100
-for i in arr:
-    if i!=-1:
-        if i in arr2:
-            x+=1
-for i in arr2:
-    if i!=-1:
-        if i in arr:
-            x+=1
-print(x//2)
+m,n=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+a=set(a)
+b=set(b)
+c=[]
+for i in a:
+    if i in b:
+        c.append(i)
+for j in b:
+    if j in a:
+        c.append(j)
+print(len(c)//2)
